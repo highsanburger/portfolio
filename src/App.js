16e597d61902109d703e7081e6a9dbc5c757c506
Syed Khalid <magmastar167@gmail.com>
2023-10-14 13:46:05 +0300

    ecclesiastes 1:2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header>
+        <h1>Syed Khalid</h1>
       </header>
+
+      <section className="about">
+        <h2>About Me</h2>
+        <p>
+          3rd Year B.Tech EEE student interested in all things quantumy and computery.
+        </p>
+      </section>
+
+      <section className="skills">
+        <h2>Skills</h2>
+        <ul>
+          <li>JavaScript</li>
+          <li>HTML & CSS</li>
+          <li>Haskell</li>
+          <li>Scientifc Computing</li>
+          <li>Quantum Physics</li>
+        </ul>
+      </section>
+
+      <section className="portfolio">
+        <h2>Portfolio</h2>
+        <div className="project">
+          <h3>Partial Differential Equations Solver</h3>
+          <p>Implemented Finite Element Method in python to solve simple PDEs.</p>
+        </div>
+        <div className="project">
+          <h3>Arch Linux pseudo-distro</h3>
+          <p>A set of GNU/Linux dotfiles aiming at creating a powerful, optimized, all-purpose and lightweight general computing environment.</p>
+        </div>
+      </section>
+
+      <section className="contact">
+        <h2>Contact</h2>
+        <p>
+          Feel free to reach out to me at imsyedkhalid@gmail.com.
+        </p>
+      </section>
     </div>
   );
 }
 
 export default App;
+
